3d265420f6b1fab476918fac8a49124b45d6d511
AskinasNatemin <askinas2002@gmail.com>
2024-08-20 10:13:45 +0530

    studentlogin20%

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
+import './Assets/Styles/StudentLogin.css'
+import StudentLogin from './Components/Students/StudentLogin';
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<StudentLogin/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
