d5409cab4b61e3ae15701cd28105ab9d4281bfaf
jophyds <dsjophy10@gmail.com>
2024-08-20 10:53:30 +0530

    'jophy'

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
+import StaffLogin from './Components/Staff/StaffLogin';
 function App() {
   return (
     <div className="App">
-      
+      <StaffLogin/>
     </div>
   );
 }
