fceed3c5c58bb23696b8a82acc02598f44f7f7c7
jerushse <jerush585@gmail.com>
2024-08-23 13:08:20 +0530

    pushing adminlog page

--- src/Components/Admin/AdminLogin.jsx
+++ src/Components/Admin/AdminLogin.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import '../../Assets/Styles/AdminLogin.css'
+import { Link } from 'react-router-dom'
 
 function AdminLogin() {
   return (
-    <>
-     <div className='rform'>
-<h2>Registration Form</h2>
-<div className='name'><label htmlFor="Name">Name: <input type="text" /></label></div>
-<div className='email'><label htmlFor="Email">Email: <input type="text" /></label></div>
-<div className='pass'><label htmlFor="pass">Password: <input type="password" /></label></div>
-<div className='phone'><label htmlFor="phone">Phone Number: <input type="number" /></label></div>
-<div className='gender'><input type="checkbox"/><label htmlFor="male">male</label> <input type="checkbox" /><label htmlFor="female">female</label> <input type="checkbox" /><label htmlFor="other">other</label></div>
-<button type='submit'>Create Account</button>
-<h4>Already have an account or<a href="">Sign in?</a></h4>
+   <div>
+    <div className='loginpage'>
+       <h2 className='alogin'>Admin Login</h2>
+       <div className='username'><input type="text" placeholder='Username'/></div>
+       <div className='email'><input type="email" placeholder='Email'/></div>
+       <div className='password'><input type="password" placeholder='Password'/></div>
+       <div className='but'><button type='submit'>Login</button></div>
+       <div className='fp'><Link>Forgetpassword?</Link></div>
+       
 
-      </div>
-    </>
+    </div>
+   </div>
   )
 }
 
